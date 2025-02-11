a77376cfef919f22347bd8a3856e64c1633eb9d3
Flurry <41346632+Flurry2005@users.noreply.github.com>
2025-02-11 18:19:40 +0100

    swapped location of remove workout button. Added new (temporary) icons for buttons in the program planner.

--- src/main/java/se/aljr/application/programplanner/ProgramPanel.java
+++ src/main/java/se/aljr/application/programplanner/ProgramPanel.java
@@ -31,6 +31,7 @@ public class ProgramPanel extends JPanel {
     private ImageIcon emptyBackground;
     private Image scaledEmptyBackground;
     private ImageIcon scaledEmptyBackgroundIcon;
+
     private ImageIcon addButton;
     private Image scaledAddButton;
     private ImageIcon scaledAddButtonIcon;
@@ -38,6 +39,36 @@ public class ProgramPanel extends JPanel {
     private ImageIcon saveButton;
     private Image scaledsaveButton;
     private ImageIcon scaledsaveButtonIcon;
+
+    private ImageIcon removeWorkoutButtonImage;
+    private Image scaledRemoveWorkoutButtonImage;
+    private ImageIcon scaledRemoveWorkoutIcon;
+
+    private ImageIcon newWorkoutButtonImage;
+    private Image scaledNewWorkoutButtonImage;
+    private ImageIcon scaledNewWorkoutIcon;
+
+    private ImageIcon removeExerciseButtonImage;
+    private Image scaledRemoveExerciseButtonImage;
+    private ImageIcon scaledRemoveExerciseIcon;
+
+    private ImageIcon newSetButtonImage;
+    private Image scaledNewSetButtonImage;
+    private ImageIcon scaledNewSetIcon;
+
+    private ImageIcon removeSetButtonImage;
+    private Image scaledRemoveSetButtonImage;
+    private static ImageIcon scaledRemoveSetIcon;
+
+    private ImageIcon moveSetUpButtonImage;
+    private Image scaledMoveSetUpButtonImage;
+    private static ImageIcon scaledMoveSetUpIcon;
+
+    private ImageIcon moveSetDownButtonImage;
+    private Image scaledMoveSetDownButtonImage;
+    private static ImageIcon scaledMoveSetDownIcon;
+
+
 
 
     public ProgramPanel(int width, int height) {
@@ -53,6 +84,34 @@ public class ProgramPanel extends JPanel {
         saveButton = new ImageIcon(resourcePath + "save_workout_button.png");
         scaledsaveButton = saveButton.getImage().getScaledInstance(140, 29, Image.SCALE_SMOOTH);
         scaledsaveButtonIcon = new ImageIcon(scaledsaveButton);
+
+        removeWorkoutButtonImage = new ImageIcon(resourcePath + "remove_workout_button.png");
+        scaledRemoveWorkoutButtonImage = removeWorkoutButtonImage.getImage().getScaledInstance(removeWorkoutButtonImage.getIconWidth(), removeWorkoutButtonImage.getIconHeight(), Image.SCALE_SMOOTH);
+        scaledRemoveWorkoutIcon = new ImageIcon(scaledRemoveWorkoutButtonImage);
+
+        newWorkoutButtonImage = new ImageIcon(resourcePath + "new_workout_button.png");
+        scaledNewWorkoutButtonImage = newWorkoutButtonImage.getImage().getScaledInstance(newWorkoutButtonImage.getIconWidth(), newWorkoutButtonImage.getIconHeight(), Image.SCALE_SMOOTH);
+        scaledNewWorkoutIcon = new ImageIcon(scaledNewWorkoutButtonImage);
+
+        removeExerciseButtonImage = new ImageIcon(resourcePath + "remove_exercise_button.png");
+        scaledRemoveExerciseButtonImage = removeExerciseButtonImage.getImage().getScaledInstance(removeExerciseButtonImage.getIconWidth(), removeExerciseButtonImage.getIconHeight(), Image.SCALE_SMOOTH);
+        scaledRemoveExerciseIcon = new ImageIcon(scaledRemoveExerciseButtonImage);
+
+        newSetButtonImage = new ImageIcon(resourcePath + "new_set_button.png");
+        scaledNewSetButtonImage = newSetButtonImage.getImage().getScaledInstance(newSetButtonImage.getIconWidth(), newSetButtonImage.getIconHeight(), Image.SCALE_SMOOTH);
+        scaledNewSetIcon = new ImageIcon(scaledNewSetButtonImage);
+
+        removeSetButtonImage = new ImageIcon(resourcePath + "remove_set_button.png");
+        scaledRemoveSetButtonImage = removeSetButtonImage.getImage().getScaledInstance(removeSetButtonImage.getIconWidth(), removeSetButtonImage.getIconHeight(), Image.SCALE_SMOOTH);
+        scaledRemoveSetIcon = new ImageIcon(scaledRemoveSetButtonImage);
+
+        moveSetUpButtonImage = new ImageIcon(resourcePath + "move_set_up.png");
+        scaledMoveSetUpButtonImage = moveSetUpButtonImage.getImage().getScaledInstance(moveSetUpButtonImage.getIconWidth(), moveSetUpButtonImage.getIconHeight(), Image.SCALE_SMOOTH);
+        scaledMoveSetUpIcon = new ImageIcon(scaledMoveSetUpButtonImage);
+
+        moveSetDownButtonImage = new ImageIcon(resourcePath + "move_set_down.png");
+        scaledMoveSetDownButtonImage = moveSetDownButtonImage.getImage().getScaledInstance(moveSetDownButtonImage.getIconWidth(), moveSetDownButtonImage.getIconHeight(), Image.SCALE_SMOOTH);
+        scaledMoveSetDownIcon = new ImageIcon(scaledMoveSetDownButtonImage);
 
         this.setSize(width, height);
         this.setBackground(new Color(31, 31, 31));
@@ -122,17 +181,16 @@ public class ProgramPanel extends JPanel {
         savedWorkoutsPanel.setBackground(new Color(51, 51, 51));
 
         JPanel savedWorkoutsPanelTop = new JPanel();
-        savedWorkoutsPanelTop.setLayout(new BorderLayout(0, 0));
+        savedWorkoutsPanelTop.setLayout(new BoxLayout(savedWorkoutsPanelTop, BoxLayout.X_AXIS));
         savedWorkoutsPanelTop.setOpaque(false);
         savedWorkoutsPanelTop.setPreferredSize(new Dimension(getWidth() / 5, getHeight() / 20));
         savedWorkoutsPanelTop.setMinimumSize(new Dimension(getWidth() / 5, getHeight() / 20));
         savedWorkoutsPanelTop.setMaximumSize(new Dimension(getWidth() / 5, getHeight() / 20));
 
         JLabel savedWorkoutsLabel = new JLabel("Workouts");
+        savedWorkoutsLabel.setMaximumSize(new Dimension(63,11));
         savedWorkoutsLabel.setFont(new Font("Arial", Font.PLAIN, 15));
         savedWorkoutsLabel.setForeground(Color.CYAN);
-        savedWorkoutsLabel.setPreferredSize(new Dimension(getWidth() / 5, getHeight() / 20));
-        savedWorkoutsLabel.setMaximumSize(new Dimension(getWidth() / 5, getHeight() / 20));
         savedWorkoutsLabel.setOpaque(false);
 
         // Select saved workouts
@@ -179,7 +237,13 @@ public class ProgramPanel extends JPanel {
 
         });
 
-        JButton newWorkoutButton = new JButton("New workout");
+        JButton newWorkoutButton = new JButton(scaledNewWorkoutIcon);
+        newWorkoutButton.setContentAreaFilled(false);
+        newWorkoutButton.setPreferredSize(new Dimension(scaledNewWorkoutIcon.getIconWidth(),scaledNewWorkoutIcon.getIconHeight()));
+        newWorkoutButton.setMaximumSize(newWorkoutButton.getPreferredSize());
+        newWorkoutButton.setBorder(null);
+        newWorkoutButton.setFocusable(false);
+        newWorkoutButton.setBorderPainted(false);
         newWorkoutButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -192,13 +256,13 @@ public class ProgramPanel extends JPanel {
             }
         });
 
-        JButton deleteWorkout = new JButton("X");
+        JButton deleteWorkout = new JButton();
         deleteWorkout.setBorder(null);
-        deleteWorkout.setText("X");
+        deleteWorkout.setContentAreaFilled(false);
+        deleteWorkout.setIcon(scaledRemoveWorkoutIcon);
         deleteWorkout.setFont(new Font("Arial", Font.BOLD, 10));
-        deleteWorkout.setPreferredSize(new Dimension(getWidth()/60, getWidth()/60));
+        deleteWorkout.setPreferredSize(new Dimension(scaledRemoveWorkoutIcon.getIconWidth(), scaledRemoveWorkoutIcon.getIconHeight()));
         deleteWorkout.setMaximumSize(deleteWorkout.getPreferredSize());
-        deleteWorkout.setBackground(Color.red);
         deleteWorkout.setBorderPainted(true);
         deleteWorkout.setFocusable(false);
         deleteWorkout.addMouseListener(new MouseAdapter() {
@@ -455,8 +519,6 @@ public class ProgramPanel extends JPanel {
 
         workoutPanelTop.add(workoutTitle);
         workoutPanelTop.add(Box.createHorizontalGlue());
-        workoutPanelTop.add(deleteWorkout);
-        workoutPanelTop.add(Box.createHorizontalGlue());
         workoutPanelTop.add(saveWorkoutButton);
 
 
@@ -466,8 +528,11 @@ public class ProgramPanel extends JPanel {
         workoutPanel.add(workoutScrollPane);
         workoutPanel.add(Box.createVerticalGlue());
 
-        savedWorkoutsPanelTop.add(savedWorkoutsLabel, BorderLayout.WEST);
-        savedWorkoutsPanelTop.add(newWorkoutButton, BorderLayout.EAST);
+        savedWorkoutsPanelTop.add(savedWorkoutsLabel);
+        savedWorkoutsPanelTop.add(Box.createHorizontalGlue());
+        savedWorkoutsPanelTop.add(deleteWorkout);
+        savedWorkoutsPanelTop.add(Box.createHorizontalGlue());
+        savedWorkoutsPanelTop.add(newWorkoutButton);
 
 
         savedWorkoutsPanel.add(Box.createVerticalGlue());
@@ -558,16 +623,16 @@ public class ProgramPanel extends JPanel {
         leftPanel.add(setLabel);
 
         //Remove exercise-button
-        JButton removeExercise = new JButton();
+        JButton removeExercise = new JButton(scaledRemoveExerciseIcon);
         removeExercise.setName("removeExercise");
         removeExercise.setPreferredSize(new Dimension(getWidth() / 15, setPanelHeight));
         removeExercise.setMinimumSize(removeExercise.getPreferredSize());
         removeExercise.setMaximumSize(removeExercise.getPreferredSize());
         removeExercise.setMargin(new Insets(0, 0, 0, 0));
         removeExercise.setForeground(Color.white);
-        removeExercise.setText("remove");
+        removeExercise.setContentAreaFilled(false);
         removeExercise.setFont(new Font("Arial", Font.BOLD, 12));
-        removeExercise.setBackground(Color.red);
+        //removeExercise.setBackground(Color.red);
         removeExercise.setBorderPainted(false);
         removeExercise.setFocusPainted(false);
         removeExercise.addActionListener(e -> {
@@ -616,16 +681,13 @@ public class ProgramPanel extends JPanel {
         rightPanel.add(rirLabel);
 
         // "Add set"- button
-        JButton addSet = new JButton();
+        JButton addSet = new JButton(scaledNewSetIcon);
         addSet.setName("addSet");
         addSet.setPreferredSize(new Dimension(getWidth() / 35, setPanelHeight));
         addSet.setMinimumSize(addSet.getPreferredSize());
         addSet.setMaximumSize(new Dimension(addSet.getPreferredSize()));
         addSet.setMargin(new Insets(0, 0, 0, 0));
-        addSet.setForeground(Color.white);
-        addSet.setText("+");
-        addSet.setFont(new Font("Arial", Font.BOLD, 12));
-        addSet.setBackground(new Color(40, 129, 201));
+        addSet.setContentAreaFilled(false);
         addSet.setBorderPainted(false);
         addSet.setFocusPainted(false);
 
@@ -691,15 +753,13 @@ public class ProgramPanel extends JPanel {
         leftPanel.add(setLabel);
 
 
-        JButton deleteSet = new JButton();
+        JButton deleteSet = new JButton(scaledRemoveSetIcon);
         deleteSet.setName("deleteSet");
-        deleteSet.setBackground(Color.RED);
-        deleteSet.setForeground(Color.WHITE);
-        deleteSet.setText("-");
+        deleteSet.setContentAreaFilled(false);
+        deleteSet.setBorder(null);
         deleteSet.setPreferredSize(new Dimension(ProgramPanel.programPanelWidth / 45, setPanelHeight));
         deleteSet.setMinimumSize(deleteSet.getPreferredSize());
         deleteSet.setMaximumSize(deleteSet.getPreferredSize());
-        deleteSet.setFont(new Font("Arial", Font.BOLD, 12));
         deleteSet.setBorderPainted(false);
         deleteSet.setFocusPainted(false);
         deleteSet.setMargin(new Insets(0, 0, 0, 0));
@@ -710,18 +770,20 @@ public class ProgramPanel extends JPanel {
 
         });
 
-        JButton moveSetUp = new JButton();
+        JButton moveSetUp = new JButton(scaledMoveSetUpIcon);
         moveSetUp.setName("moveSetUp");
-        moveSetUp.setText("↑");
-        moveSetUp.setBackground(new Color(40, 129, 201));
-        moveSetUp.setForeground(Color.white);
+        moveSetUp.setContentAreaFilled(false);
+        moveSetUp.setBorder(null);
+        moveSetUp.setBorderPainted(false);
+        moveSetUp.setFocusable(false);
         moveSetUp.setMargin(new Insets(0, 0, 0, 0));
 
-        JButton moveSetDown = new JButton();
+        JButton moveSetDown = new JButton(scaledMoveSetDownIcon);
         moveSetDown.setName("moveSetDown");
-        moveSetDown.setText("↓");
-        moveSetDown.setBackground(new Color(40, 129, 201));
-        moveSetDown.setForeground(Color.white);
+        moveSetDown.setContentAreaFilled(false);
+        moveSetDown.setBorder(null);
+        moveSetDown.setBorderPainted(false);
+        moveSetDown.setFocusable(false);
         moveSetDown.setMargin(new Insets(0, 0, 0, 0));
 
         moveSetDown.addActionListener(new ActionListener() {
